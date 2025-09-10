91fe34051f56474c91d47653103cf8fe9a23ee36
Randy Oest <oest@amazingrando.com>
2025-09-10 14:30:24 -0400

    changes

--- src/components/sections/homehero.tsx
+++ src/components/sections/homehero.tsx
@@ -2,7 +2,7 @@ export default function HomeHero() {
   return (
     <div className='px-8 py-4 md:px-16 md:py-8 flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-8xl relative'>
       <h1 className="text-[2.5rem] leading-[1] md:text-6xl lg:text-8xl font-medium text-balance">
-      I make the internet <span className="whitespace-nowrap">less ugly</span> and more useful.
+        I make digital experiences as approachable, understandable, & delightful as the real world.
       </h1>
 
       <h2 className="text-xl md:text-2xl lg:text-3xl font-medium  max-w-4xl inline-block">
