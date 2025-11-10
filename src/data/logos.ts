a6b88d50299d47baa828c78b336520fe2b07b7d8
Randy Oest <oest@amazingrando.com>
2025-11-10 12:45:10 -0500

    lint

--- src/data/logos.ts
+++ src/data/logos.ts
@@ -1,11 +1,8 @@
 import colombia from '@/assets/images/logos/colombia.svg';
 import fourKitchens from '@/assets/images/logos/fourkitchens.svg';
-import ithaca from '@/assets/images/logos/ithaca.svg';
 import nyu from '@/assets/images/logos/nyu.svg';
 import smithCollege from '@/assets/images/logos/smith-college.svg';
 import stanford from '@/assets/images/logos/stanford.svg';
-// import texasLawHelp from '@/assets/images/logos/texaslawhelp.svg';
-// import uft from '@/assets/images/logos/uft.svg';
 import yale from '@/assets/images/logos/yale.svg';
 import emulsify from '@/assets/images/logos/emulsify.svg';
 import baruch from '@/assets/images/logos/baruch.svg';
